4cae83b253751ef1c861fad232cc15e54c891660
Vijay Raghuwanshi <gvijay745@gmail.com>
2021-10-23 01:08:35 +0530

    Minor bugfix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ export default function App() {
   // })
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <LoadingBar />
       <Navbar />
       <About />
       <Projects />
